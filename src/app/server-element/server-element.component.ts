78dd8ce4b3ddb7bddde94af12d7179cc33b6b6d8
KevinRS <kevin.reindl-saldivar@datec24.ag>
2024-06-17 12:37:52 +0200

    first

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrl: './server-element.component.css'
+  styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent {
-
+  @Input('srvElement') element: { type: string, name: string, content: string };
 }
